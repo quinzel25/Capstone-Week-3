4322899cd038fefbeaeadaf18eb4b7022db42623
quinz <quinzel0325@gmail.com>
2020-09-08 17:58:39 -0500

    initial commit

---
+++ camelCase.py
@@ -0,0 +1,17 @@
+# Lab 1 Part 3
+import re
+
+
+string = input('Enter a string you want to turn into camelcase: ')
+
+# function to convert a string into camelCase
+def camelCase(snake_str):
+    components = snake_str.split(' ')
+    # capitalize the first letter of each component except the first one
+    # with the 'title' method and join them together.
+    return components[0] + ''.join(x.title() for x in components[1:])
+# This code taken from stacked overflow but the original splits it based on "_" 
+# and I changed it to work with a ' ' (blank space) to work for this instance
+
+
+print(camelCase(string))
